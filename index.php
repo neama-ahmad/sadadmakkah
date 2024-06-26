37ef15e291790c4efb26ab0b780073ee9e1e6dc6
Neama Ahmad <96380632+neama-ahmad@users.noreply.github.com>
2024-06-26 11:08:47 +0300

    upload file

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+  <meta charset="UTF8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.0/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.0/css/all.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.0/css/all.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  <link rel="icon" href="img/logo.png">
+  <title>سداد مكة لسداد القروض</title>
+  <link rel="stylesheet" href="css/boxes.css">
+  <link rel="stylesheet" href="css/main.css">
+  
+</head>
+
+<body  onload="document.body.style.opacity='1' ">
+  
+  <!--menu-->
+  <header class="header">
+    <a href="index.php"><img src="img/logocrop.png"></a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+    <li class="menu-choice social-top"><a href="tel:966543600646"  target="_blank">اتصل بنا</a></li>
+    </ul>
+  </header>
+
+  <!--cover-->
+  <div class="cover">
+    <img src="https://i.pinimg.com/originals/f8/c4/22/f8c422a0a0e6793b3f9113d419c5143a.gif">
+   
+    <h1 class="bigTitle">سداد مكة لسداد القروض
+    <a href="https://wa.me/966503436940" target="_blank" class="num"> 0503436940 </a> أو<a href="https://wa.me/966543600646"  target="_blank" class="num"> 0543600646 </a></h1>
+    <p class="inTitle">تواصل معنا وراح نساعدك في تسديد قروضك البنكية أو في تسديد إيقاف الخدمات والتعثرات البنكية والقروض القديمة وكمان في استخراج قروض جديدة ونضمن لك السرعة في الأداء وأسعار مميزة</p>
+
+    <a href="https://wa.me/966543600646" target="_blank" class="button "> <li class="fa fa-whatsapp "></li>   تواصل معنا عبر الواتساب</a>
+  </div>
+
+  <!--services-->
+  <div id="services">
+    <h2 class="Title" >خدماتنا</h2>
+
+    <div class="row">
+      <div class="column">
+        <i class="fas fa-hand-holding-usd  fa-3x icons"></i>
+        <p class="features">استخراج قرض جديد لدى جميع البنوك</p>
+      </div>
+
+      <div class="column">
+        <i class="fab fa-cc-visa fa-3x icons"></i>
+        <p class="features">تسديد قروض  بنكية</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-search-dollar fa-3x icons"></i>
+        <p class="features">سداد المتعثرات لدى سمه</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-gavel fa-3x icons"></i>
+        <p class="features">سداد إيقاف الخدمات لدى المحاكم</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-university fa-3x icons"></i>
+        <p class="features">التحويل من بنك إلى بنك آخر</p>
+      </div>
+
+      <div class="column">
+        <i class="fas fa-home fa-3x icons"></i>
+        <p class="features">توفير دفعه و ضريبة للقرض العقاري لنزول القرض</p>
+      </div>
+
+    </div>
+  </div>
+
+
+
+  <!--contact-->
+  <div id ="contact">
+    <h2 class="Title">تواصل معنا</h2>
+    <!--social media buttons-->
+    <div class="banner" id="banner1">
+      <a href= "mailto:humud505@gmail.com" class="far fa-envelope email"></a>
+      <a href="https://wa.me/966503436940" target="_blank" class="fa fa-whatsapp"></a>
+      <a href="https://www.facebook.com/profile.php?id=100070357513607" target="_blank" class="fa fa-facebook"></a>
+      <a href="https://twitter.com/sadadmakkah" target="_blank" class="fa fa-twitter"></a>
+    </div>
+
+  </div>
+
+
+  <!--client-->
+  <div class="info" id="client">
+    <div class="imgwrapper">
+      <div class="imgAd"><img src="img/ahly.png"></div>
+      <div class="imgAd2"><img src="img/rajihi.png"></div>
+      <div class="imgAd"><img src="img/ryadh.png"></div>
+    </div>
+  </div>
+  
+  <!--footer-->
+  <div  class="copyright">
+    <p> جميع الحقوق محفوظه &copy; <span id="year"></span>&nbsp;<a href="index.php">لسداد مكة</a></p>
+    <script>document.getElementById("year").innerHTML=(new Date().getFullYear())</script>
+  </div>
+
+  <script> 
+  var url = 'https://widget.bot.space/js/widget.js';
+   var s = document.createElement('script'); 
+   s.type = 'text/javascript'; 
+   s.async = true; 
+   s.src = url; 
+   var options = {"enabled":true,"chatButtonSetting":{"backgroundColor":"#13C656","ctaText":"تواصل معنا","borderRadius":"25","marginLeft":"20","marginBottom":"20","marginRight":"20","position":"right"},"brandSetting":{"brandName":"سداد مكة","brandSubTitle":"خدمة العملاء","brandImg":"https://sadadmakkah.com/img/logocrop.png","welcomeText":"مرحبا\nكيف نقدر نخدمك؟","messageText":"مرحبا\nعندي استفسار","backgroundColor":"#085E54","ctaText":"أبدأ المحادثة","borderRadius":"25","autoShow":false,"phoneNumber":"966503436940"}}; 
+   s.onload = function() { CreateWhatsappChatWidget(options); }; 
+   var x = document.getElementsByTagName('script')[0]; 
+   x.parentNode.insertBefore(s, x); 
+
+  </script>
+
+
+
+</body>
+
+</html>
